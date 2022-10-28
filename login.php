33dc4ef19a9a686b8d3a9a3aa2a08c8f913fe6f8
prodige667 <alanfontaine335@gmail.com>
2022-10-28 15:31:37 +0400

    login

---
+++ login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+    <div class="flex">
+        <h1 class="Login">Connexion</h1>
+        <form class="Nom"action="">
+            <label for="">Mail</label>
+            <input name="Mail"placeholder="Entrer votre email" type="email">
+            <label for="">Mot de passe</label>
+            <input name="Mot de passe"placeholder="Entrer votre mot de passe" type="password">
+            <a href="#"></a>
+            
+            
+            <div class="bouton">
+                <button class="mdl-button mdl-js-button mdl-button--raised"> Connexion
+                    
+                    
+            </div>    
+                
+            
+                
+            
+            
+            
+           
+    
+        
+        </form>
+
+    </div>
+    
+</body>
+</html>
