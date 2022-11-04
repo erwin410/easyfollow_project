aad2c98cc2f037450bd9cb081a03c626b6e31dea
lvsprod <erwinpateux@gmail.com>
2022-11-04 13:39:47 +0400

    Sidebar Styling

--- client/js/script.js
+++ client/js/script.js
@@ -1,14 +1,14 @@
-const api_url = 'http://localhost:8000/api/show.php?id=1';
+const api_url = 'http://localhost:8000/api/show.php?id=6';
 
 async function getUsers()
 {
     const response = await fetch(api_url);
     const data = await response.json();
-    const { name, pseudo } = data;
+    const { pseudo } = data;
 
-       
-    document.getElementById('name').textContent = name;
     document.getElementById('pseudo').textContent = pseudo;
 }
 
 getUsers();
+
+
